0664108a2d9d1244d547dcabe8072eccf339a647
tab <tukhfatullin@gmail.com>
2018-02-24 22:52:44 +0700

    Fix flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  private
+  
+  def after_sign_in_path_for(resource)
+    manage_root_path
+  end
+
 end
