6ef1fa707daf72f5ec3d0df56f177975bf58b212
tab <tukhfatullin@gmail.com>
2018-02-24 16:41:33 +0700

    Cleanup uploads

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,23 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     Warden.test_mode!
+
+    Dir["#{Rails.root}/app/uploaders/**/*.rb"].each{ |f| require f }
+    CarrierWave::Uploader::Base.descendants.each do |klass|
+      next if klass.anonymous?
+
+      klass.class_eval do
+        storage :file
+
+        def cache_dir
+          'test_uploads/tmp'
+        end
+
+        def store_dir
+          "test_uploads/#{model.class.to_s.underscore}/#{model.id}"
+        end
+      end
+    end
   end
 
   config.before(:each) do
@@ -53,7 +70,9 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
     Warden.test_reset!
+  end
 
-    FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads/tmp"])
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/public/test_uploads"])
   end
 end
